fb10f1af1a8146955ba431b408a6cac4da1cf2cd
JOELNATHAN544 <wankojoelnathan@gmail.com>
2025-02-25 15:40:12 +0100

    Update: Updated the main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "my_rust_action")]
+#[command(about = "A GitHub Action written in Rust", long_about = None)]
+struct Cli {
+    #[arg(long, default_value_t = false)]
+    enable_fib: bool,
+
+    #[arg(long, default_value_t = 100)]
+    max_threshhold: u32,
 }
+
+fn main() {
+    let args = Cli::parse();
+    println!("Verbose: {}", args.enable_fib);
+    println!("Limit: {}", args.max_threshhold);
+}
+
