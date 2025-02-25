b885fbaf67ca4812619531eb514dd74930b6e351
JOELNATHAN544 <wankojoelnathan@gmail.com>
2025-02-25 16:18:47 +0100

    fix: Modified the action.yml to take parameters
    Signed-off-by: JOELNATHAN544 <wankojoelnathan@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use clap::Parser;
+use std::env::{self, args};
+
+use clap::{builder::Str, Parser};
 
 #[derive(Parser, Debug)]
 #[command(name = "my_rust_action")]
@@ -12,8 +14,15 @@ struct Cli {
 }
 
 fn main() {
-    let args = Cli::parse();
-    println!("Verbose: {}", args.enable_fib);
-    println!("Limit: {}", args.max_threshhold);
+    let arg = Cli::parse();
+    let args: Vec<String> = env::args().collect();
+    let enable_fib = &args[0];
+    let max_threshhold = &args[1];
+    if args.len() < 2 {
+        println!("Please enter two arguments <enable_fib> <max_threshhold>");
+    } else {
+        
+        println!("Verbose: {}", enable_fib);
+        println!("Limit: {}", max_threshhold);
+    }
 }
-
