6f32fb81a0caa22a9c14ae145d474aa59d534903
JOELNATHAN544 <wankojoelnathan@gmail.com>
2025-02-27 12:31:45 +0100

    fix: Rmoved unsused lines in the scritpt.sh
    Signed-off-by: JOELNATHAN544 <wankojoelnathan@gmail.com>

--- script.sh
+++ script.sh
@@ -4,14 +4,5 @@
 echo "Test Case 1: Valid parameters"
 /app/target/release/fibbot true 100
 
-# Test case 2: Invalid enable_fib (not a boolean)
-echo "Test Case 2: Invalid enable_fib (not a boolean)"
-/app/target/release/fibbot invalid_value 100
 
-# Test case 3: Invalid max_threshhold (not a positive integer)
-echo "Test Case 3: Invalid max_threshhold (not a positive integer)"
-/app/target/release/fibbot true invalid_value
 
-# Test case 4: Missing parameters
-echo "Test Case 4: Missing parameters"
-/app/target/release/fibbot
